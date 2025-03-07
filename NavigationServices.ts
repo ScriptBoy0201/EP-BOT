def9b0349b523c5443fad884ad5b16c31fa24fed
ScriptBoy0201 <50424@immanuel.qld.edu.au>
2025-03-08 09:41:08 +1000

    Update NavigationServices.ts

--- NavigationServices.ts
+++ NavigationServices.ts
@@ -1,17 +1,25 @@
-import { Injectable } from '@angular/core';
-import { $location } from 'angular';
+import { Injectable, Inject } from '@angular/core';
+import { ILocationService } from 'angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NavigationService {
-  constructor(private $location: ng.ILocationService) {}
+  constructor(@Inject('$location') private $location: ILocationService) {}
 
-  goToUrl(url: string, params?: any) {
-    if (params) {
-      // Correct use of $location.search without arguments
-      this.$location.search({});
+  goToUrl(url: string, params?: Record<string, string | number | boolean>) {
+    try {
+      if (params && Object.keys(params).length > 0) {
+        this.$location.search(params); // Apply query parameters properly
+      } else {
+        this.$location.search({}); // Clears query params when no params are provided
+      }
+
+      this.$location.url(url);
+      console.log(`Navigated to: ${url} with params:`, params || 'No Params');
+    } catch (error) {
+      console.error('Navigation Error:', error);
     }
-    this.$location.url(url);
   }
 }
+
